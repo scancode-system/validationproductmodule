50f0df03cf6a2b945c7462da7a708f0d29780621
Your Name <you@example.com>
2019-12-28 00:29:01 -0300

    .

--- Database/Migrations/2019_05_25_045143_insert_validation_product.php
+++ Database/Migrations/2019_05_25_045143_insert_validation_product.php
@@ -21,6 +21,7 @@ class InsertValidationProduct extends Migration
             'video' => 'https://www.youtube.com/embed/zpOULjyy-n8?rel=0',
             'file' => 'produtos.xlsx',
             'validation' => 'products', 
+            'import' => 'Product@import'
         ]);
     }
 
