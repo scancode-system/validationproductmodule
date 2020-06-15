30c43f35e040ab1f94f4b43f268fdbb92eb8e024
Your Name <you@example.com>
2020-06-15 12:39:45 -0300

    .

--- Providers/ValidationProductServiceProvider.php
+++ Providers/ValidationProductServiceProvider.php
@@ -9,6 +9,16 @@ use Illuminate\Support\Facades\View;
 class ValidationProductServiceProvider extends ServiceProvider
 {
     /**
+     * @var string $moduleName
+     */
+    protected $moduleName = 'ValidationProduct';
+
+    /**
+     * @var string $moduleNameLower
+     */
+    protected $moduleNameLower = 'validationproduct';
+
+    /**
      * Boot the application events.
      *
      * @return void
@@ -38,11 +48,18 @@ class ValidationProductServiceProvider extends ServiceProvider
     protected function registerConfig()
     {
         $this->publishes([
+            module_path($this->moduleName, 'Config/config.php') => config_path($this->moduleNameLower . '.php'),
+        ], 'config');
+        $this->mergeConfigFrom(
+            module_path($this->moduleName, 'Config/config.php'), $this->moduleNameLower
+        );
+
+        /*$this->publishes([
             __DIR__.'/../Config/config.php' => config_path('validationproduct.php'),
         ], 'config');
         $this->mergeConfigFrom(
             __DIR__.'/../Config/config.php', 'validationproduct'
-        );
+        );*/
     }
 
     /**
@@ -52,7 +69,17 @@ class ValidationProductServiceProvider extends ServiceProvider
      */
     public function registerViews()
     {
-        $viewPath = resource_path('views/modules/validationproduct');
+                $viewPath = resource_path('views/modules/' . $this->moduleNameLower);
+
+        $sourcePath = module_path($this->moduleName, 'Resources/views');
+
+        $this->publishes([
+            $sourcePath => $viewPath
+        ], ['views', $this->moduleNameLower . '-module-views']);
+
+        $this->loadViewsFrom(array_merge($this->getPublishableViewPaths(), [$sourcePath]), $this->moduleNameLower);
+
+        /*$viewPath = resource_path('views/modules/validationproduct');
 
         $sourcePath = __DIR__.'/../Resources/views';
 
@@ -62,7 +89,18 @@ class ValidationProductServiceProvider extends ServiceProvider
 
         $this->loadViewsFrom(array_merge(array_map(function ($path) {
             return $path . '/modules/validationproduct';
-        }, \Config::get('view.paths')), [$sourcePath]), 'validationproduct');
+        }, \Config::get('view.paths')), [$sourcePath]), 'validationproduct');*/
+    }
+
+    private function getPublishableViewPaths(): array
+    {
+        $paths = [];
+        foreach (\Config::get('view.paths') as $path) {
+            if (is_dir($path . '/modules/' . $this->moduleNameLower)) {
+                $paths[] = $path . '/modules/' . $this->moduleNameLower;
+            }
+        }
+        return $paths;
     }
 
 }
